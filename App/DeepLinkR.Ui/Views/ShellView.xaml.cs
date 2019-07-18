7517ae42e648ec3904c3148fb573c6e463f1a0c7
steven <steven.england@t-online.de>
2019-07-18 22:05:38 +0200

    Added draggability

--- App/DeepLinkR.Ui/Views/ShellView.xaml.cs
+++ App/DeepLinkR.Ui/Views/ShellView.xaml.cs
@@ -23,5 +23,24 @@ namespace DeepLinkR.Ui.Views
 		{
 			this.InitializeComponent();
 		}
+
+		private void ColorZone_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (e.LeftButton == MouseButtonState.Pressed)
+			{
+				if (this.WindowState == System.Windows.WindowState.Maximized)
+				{
+					var x = e.GetPosition(this).X;
+					var y = e.GetPosition(this).Y;
+					var widthRatio = x / this.ActualWidth;
+					var heightRatio = y / this.TitleBar.ActualHeight;
+					this.WindowState = System.Windows.WindowState.Normal;
+					this.Left = x - Convert.ToInt32(this.ActualWidth * widthRatio);
+					this.Top = y - Convert.ToInt32(this.TitleBar.ActualHeight * heightRatio);
+				}
+
+				this.DragMove();
+			}
+		}
 	}
 }
